9d368d125e07fcb9d21b5415589b8ef02e7b1773
CarlosCastanedaDev <carlos@nemin.dev>
2023-10-12 15:16:46 -0500

    posts and liked photos links work

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ApplicationRecord
   has_many(:sent_follow_requests, class_name: "FollowRequest", foreign_key: :sender_id)
   has_many(:received_follow_requests, class_name: "FollowRequest", foreign_key: :recipient_id)
   has_many(:likes, foreign_key: :fan_id)
-  has_many(:liked_photos, through: :likes, source: :photos)
+  has_many(:liked_photos, through: :likes, source: :photo)
   has_many(:leaders, through: :accepted_sent_follow_requests, source: :recipient)
   has_many(:accepted_sent_follow_requests, -> { accepted }, class_name: "FollowRequest", foreign_key: :sender_id)
   has_many(:accepted_received_follow_requests, -> { accepted }, class_name: "FollowRequest", foreign_key: :recipient_id)
